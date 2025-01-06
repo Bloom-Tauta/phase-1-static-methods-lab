9d477afc5874cc1a6f0b9ec39ff310d950dabb04
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2025-01-06 12:32:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(str) {
+    const exclusions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    return str
+      .split(" ")
+      .map((word, index) => {
+        const lowerWord = word.toLowerCase();
+        // Always capitalize the first word in the sentence
+        if (index === 0 || !exclusions.includes(lowerWord)) {
+          return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+        }
+        return lowerWord;
+      })
+      .join(" ");
+  }
 }
+
+
+let str = 'a tale of two cities'
+console.log(Formatter.capitalize(str))
